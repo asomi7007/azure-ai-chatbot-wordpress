de1cb71bfac0b21fcdbc67c6a247b4f3364a6501
asomi7007 <asomi@naver.com>
2025-10-26 23:33:05 +0900

    Release v2.5.0 - JavaScript 구문 오류 최종 수정 확인

--- create-zip.ps1
+++ create-zip.ps1
@@ -2,7 +2,7 @@
 # This script creates a distribution ZIP file for the plugin
 
 param(
-    [string]$Version = "2.4.2"
+    [string]$Version = "2.5.0"
 )
 
 $ZipFileName = "azure-ai-chatbot-wordpress-$Version.zip"
